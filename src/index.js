accbfd0fd7337b306260c9b0578cb3e33a8d99f6
Haseeb Majid <hello@haseebmajid.dev>
2020-10-01 08:22:12 +0100

    chore: format index.js (#4)
    Use prettier to format the `index.js` file.
    
    Co-authored-by: Haseeb Majid <haseeb.majid@sky.uk>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const backdropFilters = require("./plugins/backdropFilters");
-const filters = require("./plugins/filters");
+const backdropFilters = require('./plugins/backdropFilters')
+const filters = require('./plugins/filters')
 
 module.exports = ({ addUtilities, theme, e }) => {
-    backdropFilters({ addUtilities, theme, e });
-    filters({ addUtilities, theme, e });
+  backdropFilters({ addUtilities, theme, e })
+  filters({ addUtilities, theme, e })
 }
